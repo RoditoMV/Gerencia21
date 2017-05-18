2eb11d2d023f79a6db9ab74d0cdb7694be173253
Rodolfo Montes <rodolfomonval@gmail.com>
2017-05-18 15:38:05 -0500

    Conformando los archivos de la página GERENCIA 21

---
+++ gerencia21/js/interacciones.js
@@ -0,0 +1,13 @@
+//////////////////////////// INICIA CÓDIGO PARA EL SLIDER//////////////////
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        touch: true,
+        pauseOnAction: false,
+        pauseOnHover: true,
+        slideshowSpeed: 5000,
+    });
+});
+//////////////////////////// FINALIZA CÓDIGO PARA EL SLIDER//////////////////
+//////////////////////////// INICIA CÓDIGO PARA EL SLIDER//////////////////
+
+//////////////////////////// FINALIZA CÓDIGO PARA EL SLIDER//////////////////
